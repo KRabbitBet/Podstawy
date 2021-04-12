3de9f3c1948fac64444ebdc944b94e040da1aa85
Beata <beatahumeniuk@gmail.com>
2021-04-12 20:44:52 +0200

    Update Equals.java

--- src/com/company/Equals.java
+++ src/com/company/Equals.java
@@ -15,9 +15,18 @@ public class Equals {
         Supplier<Object> objectCreator = Object::new;
         Object objectInstance = objectCreator.get();
         System.out.println(objectInstance);
+//  DO zastanowienia:
+//          Dlaczego w tych przypadkach mamy false?
+//          W jaki sposób equals porównuje obiekty?
+//          Jaka jest różnica między equals i ==
+//          Na czym polega kontrakt equals-hashCode?
+//
+//        Oraz:
+//              stwórz logikę, ktora porówna dwa takie same obiekty i zwróci true
 
         BiPredicate<Object, Object> equalsMethodOnClass = Object::equals;
         System.out.println(equalsMethodOnClass.test(objectInstance, new Object()));
+
 
         Predicate<Object> equalsMethodOnObject = objectInstance::equals;
         System.out.println(equalsMethodOnObject.test(new Object()));
